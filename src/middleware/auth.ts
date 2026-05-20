c090cd94927cd68f1ca2e21b3a307fd3591c830d
Minimal-Alexi <alexpop@metropolia.fi>
2026-05-20 07:24:11 +0300

    Renamed requireAuth to requireAuthentication. Created requireAuthorization function.

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,10 +1,11 @@
 import { Request, Response, NextFunction } from "express";
+import { findUserById } from "../service/user.service";
 
-export const requireAuth = (req: Request, res: Response, next: NextFunction) => {
+export const requireAuthentication = (req: Request, res: Response, next: NextFunction) => {
   const { authorization } = req.headers;
 
   if (!authorization) {
-    return res.status(401).json({ error: "Authorization token required" });
+    return res.status(401).json({ error: "Authentication token required" });
   }
 
   const token = authorization.split(" ")[1];
@@ -18,6 +19,23 @@ export const requireAuth = (req: Request, res: Response, next: NextFunction) => 
     next();
   } catch (error) {
     console.error(error);
-    return res.status(401).json({ error: "Invalid authorization token" });
+    return res.status(401).json({ error: "Invalid Authentication token" });
+  }
+}
+
+export const requireAuthorization = async (req:Request, res:Response, next: NextFunction) => {
+  try {
+    const token = (req as any).body.userID
+    const user = await findUserById(token)
+    if (!token || !user) {
+      return res.status(401).json({ error: "Authentication token not found." })
+    }
+    if(!user.admin){
+      return res.status(403).json({error: "You do not have permission for this command."})
+    }
+    next()
+  } catch (error) {
+    console.error(error)
+    return res.status(500).json({error:"Error authorizing request."})
   }
 }
