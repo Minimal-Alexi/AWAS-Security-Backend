76a6e4c8a95fcf7f6f28aff14eae3a2dc6937391
Minimal-Alexi <alexpop@metropolia.fi>
2026-05-20 10:38:13 +0300

    Finished getAllSupportChats.

--- src/controllers/chat.controller.ts
+++ src/controllers/chat.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import { createSupportChat, findChatById, getAllMyChats } from '../service/chat.service';
+import { createSupportChat, findChatById, getAllChats, getAllMyChats } from '../service/chat.service';
 import { findUserById } from '../service/user.service';
 import { createMessageForChat } from '../service/message.service';
 
@@ -50,10 +50,6 @@ export const createAdminMessage = async(req:Request, res:Response) => {
             return res.status(404).json({message:"Not Found"});
         }
 
-        if(!user.admin){
-            return res.status(403).json({message:"Unauth"});;
-        }
-
         const message = await createMessageForChat(chatId,user,req.body.text);
         return res.status(201).json({message:"Successfully created message.", supportMessage: message.toJSON()})
 
@@ -93,8 +89,10 @@ export const getMySupportChats = async(req:Request, res:Response) => {
 }
 export const getAllSupportChats = async(req:Request, res:Response) => {
     try {
-
+        const supportChats = await getAllChats();
+        return res.status(200).json({message:"Successfully found chats.", supportChats: supportChats.map((chat) => chat.toJSON())})
     } catch (error) {
-        return res.status(500).json({ error: "Server error." })
+        console.error(error);
+        return res.status(500).json({ error: "Server bullshit."})
     }
 }
