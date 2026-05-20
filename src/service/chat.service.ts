2ea1ade52ea2fcbd80fe7b16358a714037a0d325
Minimal-Alexi <alexpop@metropolia.fi>
2026-05-20 08:50:03 +0300

    Finished findChatById Finished getAllChats Finished getAllMyChats Created all message services.

--- src/service/chat.service.ts
+++ src/service/chat.service.ts
@@ -1,13 +1,47 @@
-const pool = require('../config/database').pool;
-export const findChatById = async(id:number) => {
+import { Chat } from "../models/chat.model";
+import { getAllMessagesFromChat } from "./message.service";
 
+const pool = require('../config/database').pool;
+export const findChatById = async(id:number):Promise<Chat | null> => {
+    const result = await pool.query('SELECT * FROM support_chat WHERE support_chat_id = $1', [id]);
+    const row = result.rows[0];
+
+    if(!row){
+        return null;
+    }
+    
+    const messages = await getAllMessagesFromChat(id);
+    return new Chat(
+        row.user_id,
+        row.support_chat_id,
+        messages
+    )
 }
 export const createSupportChat = async() => {
 
 } 
-export const getAllMyChats = async() => {
+export const getAllMyChats = async(userId:number) => {
+    const result = await pool.query('SELECT * FROM support_chat WHERE user_id = $1',
+        [userId]
+    )
+
+    const chatsArray:Array<Chat> = []
+
+    for(const chat of result.rows){
+        chatsArray.push(new Chat(chat.user_id,chat.support_chat_id,[]))
+    }
+
+    return chatsArray;
 
 }
-export const getAllChats = async() => {
+export const getAllChats = async():Promise<Array<Chat>> => {
+    const result = await pool.query('SELECT * FROM support_chat');
 
+    const chatsArray:Array<Chat> = []
+
+    for(const chat of result.rows){
+        chatsArray.push(new Chat(chat.user_id,chat.support_chat_id,[]))
+    }
+
+    return chatsArray;
 }
--- src/service/message.service.ts
+++ src/service/message.service.ts
@@ -1,14 +1,56 @@
 import { Message } from "../models/message.model";
 import { User } from "../models/user.model";
+import { findUserById } from "./user.service";
 
 const pool = require('../config/database').pool;
 export const getAllMessagesFromChat = async (id:number):Promise<Array<Message>> => {
+    const result = await pool.query(`SELECT 
+            m.message_id,
+            m.support_chat_id,
+            m.user_id,
+            m.text,
+            m.order_nr,
+            u.first_name,
+            u.last_name,
+            u.admin,
+            FROM messages m
+            JOIN users u ON u.id = m.user_id
+            WHERE m.support_chat_id = $1
+            ORDER BY m.order_nr ASC;`, [id]);
+
     const chatMessages:Array<Message> = [];
+    for(const message of result){
+        const user = new User(message.user_id,message.first_name,message.last_name,"irrelevant","irrelevant",0,message.admin);
+        chatMessages.push(new Message(message.message_id,message.support_chat_id,user,message.order,message.text))
+    }
     return chatMessages;
 }
 export const findMaxOrderFromChat = async(chatId:number):Promise<number> => {
-    return 0;
+      const orderResult = await pool.query(
+    `
+    SELECT COALESCE(MAX(order_nr), 0) + 1 AS next_order
+    FROM messages
+    WHERE support_chat_id = $1;
+    `,
+    [chatId]
+  );
+
+  const orderNr = orderResult.rows[0].next_order;
+  return orderNr
 }
-export const createMessageForChat = async(chatId:number,userId:number,text:string):Promise<Message> => {
+export const createMessageForChat = async(chatId:number,user:User,text:string):Promise<Message> => {
+    const order = await findMaxOrderFromChat(chatId);
+    const result = await pool.query(`
+        INSERT INTO message (support_chat_id,user_id,order_nr,text) VALUES ($1, $2, $3, $4) RETURNING *
+        `,[chatId,user.id,order,text])
     
+    const row = result.rows[0]
+
+    return new Message(
+        row.message_id,
+        row.support_chat_id,
+        user,
+        row.order_nr,
+        row.text
+    )
 }
